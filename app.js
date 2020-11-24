541db73c96bcc647b5089b7f3ddfb7e8006c9838
Guilherme Brandão <guilherme1237@gmail.com>
2020-11-24 17:54:52 -0300

    User Authentication with Mongoose and JWT

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const healthRouter = require('./routes/health');
+const authRouter = require('./routes/auth');
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected!")
+})
+.catch(err => console.log(err))
 
 const app = express();
 
@@ -14,6 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('[:date[clf]] | ":method :url HTTP/:http-version" | STATUS: :status | CONTENT_LENGTH: :res[content-length] | RESPONSE_TIME: :response-time ms'));
 app.use(healthRouter)
+app.use("/auth", authRouter)
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Not Found!</h1>')
---
+++ routes/auth.js
@@ -0,0 +1,63 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+
+router.post('/signup', (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            })
+            user.save().then(result => {
+                res.status(201).json({
+                    message: 'User Created!',
+                    result: result
+                })
+            })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    })
+                })
+        })
+})
+
+router.post('/login', (req, res, next) => {
+    let userData = {}
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: "User is not signed!"
+                })
+            }
+            userData = user
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: "Auth failed!"
+                })
+            }
+            //Criar um token baseado nos dados que desejar -> nesse caso sao os dados de login
+            const token = jwt.sign({ email: userData.email, userId: userData._id },
+                'secret_for_authentication', { expiresIn: "1h" })
+            res.status(200).json({
+                token: token,
+                expiresIn: 3600
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({
+                message: "Invalid Authentication!"
+            })
+
+        })
+})
+
+module.exports = router
